95df18a108cdd52ecae14b730c0689defbd2a3b1
Valentina Blasone <72449378+valebl@users.noreply.github.com>
2022-11-21 10:23:11 +0000

    Minor change

--- Lezione2/sum_list.py
+++ Lezione2/sum_list.py
@@ -10,12 +10,12 @@ def sum_list(my_list):
     return sum_list
 
 # Comment the following lines to test the program with Autograding
-# if __name__ == '__main__':
+if __name__ == '__main__':
 
-#     list1 = [1,2,3,4,5,6,7,8,9, 10]
-#     list2 = []
-#     sum_list1 = sum_list(list1)
-#     sum_list2 = sum_list(list2)
+    list1 = [1,2,3,4,5,6,7,8,9, 10]
+    list2 = []
+    sum_list1 = sum_list(list1)
+    sum_list2 = sum_list(list2)
         
-#     print("The sum of {} is {}.".format(list1, sum_list1))
-#     print("The sum of {} is {}.".format(list2, sum_list2))
+    print("The sum of {} is {}.".format(list1, sum_list1))
+    print("The sum of {} is {}.".format(list2, sum_list2))
